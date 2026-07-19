f79a629283067f972de51143ac0aaf72af0d40ea
Alper Cem <56734989+aalpercem@users.noreply.github.com>
2026-07-19 16:13:45 +0300

    feat: add supermaven-nvim completion plugin
    Tab accept, C-] clear, C-j word accept. Ignore filetypes empty (all).

--- .config/nvim/lua/specter/plugins/init.lua
+++ .config/nvim/lua/specter/plugins/init.lua
@@ -21,4 +21,5 @@ return {
   { import = 'specter.plugins.nvimtree' },
   { import = 'specter.plugins.snacks' },
   { import = 'specter.plugins.love2d' },
+  { import = 'specter.plugins.supermaven' },
 }
